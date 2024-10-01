ff31e281cf8d3f9aff193125ba0e5a3f60565395
raniabht <raniabht@hotmail.com>
2024-10-01 18:17:56 +0200

    resV0

--- gsap.js
+++ gsap.js
@@ -1,17 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     gsap.to(".partRech", {x: 50});
     gsap.from(".partillubanner", {x: 200});
-});
-
-
-    // document.addEventListener("DOMContentLoaded", function() {
-    //     gsap.from(".gsap", {y: 100, // any properties (not limited to CSS)
-    //         opacity:0,});
-    // });
-     
-    // Assurez-vous que le DOM est chargé avant d'exécuter le script
-    document.addEventListener("DOMContentLoaded", function () {
-        gsap.registerPlugin(ScrollTrigger); // Enregistrer le plugin ScrollTrigger
+    gsap.registerPlugin(ScrollTrigger); // Enregistrer le plugin ScrollTrigger
     
         // Créez une timeline GSAP
         const tl = gsap.timeline({
@@ -22,7 +12,11 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     
         // Ajoutez les animations pour chaque élément
-        tl.from(".gsap1", { duration: 0.2, opacity: 0, y: 50 }) // Anime l'élément 1
-          .from(".gsap2", { duration: 0.5, opacity: 0, y: 50 }, "<") // Anime l'élément 2 après l'élément 1
-          .from(".gsap3", { duration: 0.8, opacity: 0, y: 50 }, "<"); // Anime l'élément 3 après l'élément 2
+        // Ajoutez les animations pour tous les éléments avec un effet de stagger
+    tl.from(".gsap", { 
+        duration: 0.5, 
+        opacity: 0, 
+        y: 50, 
+        stagger: 0.2 // Délai entre chaque animation
+    });
     });
